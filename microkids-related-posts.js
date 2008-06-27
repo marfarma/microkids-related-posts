351abb82f240980cd296a8990028fea00330dc65
microkid <microkid@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-06-27 10:27:52 +0000

    Tiny bug in the Javascript
    git-svn-id: http://svn.wp-plugins.org/microkids-related-posts/tags/1.0@52144 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- microkids-related-posts.js
+++ microkids-related-posts.js
@@ -5,7 +5,7 @@ var MRP_search_tool_visible = false;
 jQuery(document).ready(function($){  
 	$("#MRP_search").keyup(function(){
 		if( $("#MRP_search").val() != '' ) {
-			$("#MRP_results").load("/wp-content/plugins/microkids-related-posts/mrp-search.php?mrp_s=" + escape( $("#MRP_search").val() ), '', 
+			$("#MRP_results").load("../wp-content/plugins/microkids-related-posts/mrp-search.php?mrp_s=" + escape( $("#MRP_search").val() ), '', 
 				function() { $("#MRP_results li .MRP_result").each(function(i) {
 						$(this).click(function() {
 
